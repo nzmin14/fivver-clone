e91c927ce87c60882487eb8f8cea344149948918
nzmin714 <minian714@gmail.com>
2024-06-27 17:47:13 +1000

    auth and database

---
+++ components/ui/auth/loading.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image"
+
+export const Loading = () => {
+    return (
+        <div className="h-full w-full flex justify-center items-center">
+            <Image src="/next.svg" alt="loading" width={120} height={120} className="animate-pulse duration-700" />
+        </div>
+    )
+
+}
---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  providers: [
+    {
+      domain: "https://loyal-monkfish-49.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ]
+};
